7ba6bd331372cd2cd81bc05a72cac7d99c51a085
schulle334 <62450965+schulle334@users.noreply.github.com>
2024-09-16 20:12:07 +0200

    Delete scripts/cameraCheck.lua

--- scripts/cameraCheck.lua
+++
@@ -1,5 +0,0 @@
-
-
-function AdjustVehicleSound:isInsideCamera(camera)
-    return camera.isInside or (camera.name and (string.find(camera.name, "interior") or string.find(camera.name, "inside")))
-end
